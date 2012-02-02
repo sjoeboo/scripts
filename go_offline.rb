edd1695b828bcb86d723945eb25388efe343a3a6
Matthew Nicholson <sjoeboo@nichols2macbookpro.(none)>
2012-02-02 08:52:22 -0500

    add go_offline script

---
+++ go_offline.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+#
+#Simple script to bring a bunch of repos (svn and git) uptodate
+#
+require 'git'
+
+svn_repos=Array.new
+git_repso=Array.new
+
+svn_repos=["puppet"]
+git_repos=["admin","docs","sjoeboo.github.com","dotfiles"]
+
+svn_repos.each do |svn_repo|
+	svn_cmd = "svn up #{svn_repo}"
+	puts "Now Updating repo: #{svn_repo}"
+	output = %x[#{svn_cmd}]
+	puts output
+	#output.each do |out|
+	#	puts out.chomp!
+	#end
+end
+
+git_repos.each do |git_repo|
+	#Do stuff, hopefully with git bindings!
+	puts "Now Updating repo: #{git_repo}"
+	#g = Git.open(git_repo)
+	#g.pull
+	git_cmd = "cd #{git_repo} && git pull"
+	output = %x[#{git_cmd}]
+	puts output
+end
